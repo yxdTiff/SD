dcba2519e49af44a51bf7b6d3b66b2b6f5bf7b60
Yifan Liu <imliuyifan@gmail.com>
2013-10-30 13:13:31 -0700

    Initial Commit

---
+++ sdlb/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  sdlb
+//
+//  Created by Yifan Liu on 10/30/13.
+//  Copyright (c) 2013 Yifan Liu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
