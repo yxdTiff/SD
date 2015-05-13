9caf8f3b9a9ba15fe1dbdde781c08f63cff67c9c
Yifan Liu <imliuyifan@gmail.com>
2015-05-13 12:00:21 -0700

    let's start here.

---
+++ DerivedData/sdlb/Build/Intermediates/sdlb.build/Debug/sdlb.build/Objects-normal/x86_64/rvgen.d
@@ -0,0 +1,7 @@
+dependencies: /Users/yifanliu/Developer/sd/sdlb/rvgen.c \
+  /Users/yifanliu/Developer/sd/sdlb/prob.h \
+  /Users/yifanliu/Developer/sd/sdlb/sdconstants.h \
+  /Users/yifanliu/Applications/IBM/ILOG/CPLEX_Studio126/cplex/include/ilcplex/cplex.h \
+  /Users/yifanliu/Applications/IBM/ILOG/CPLEX_Studio126/cplex/include/ilcplex/cpxconst.h \
+  /Users/yifanliu/Developer/sd/sdlb/sdglobal.h \
+  /Users/yifanliu/Developer/sd/sdlb/rvgen.h
---
+++ DerivedData/sdlb/Build/Intermediates/sdlb.build/Debug/sdlb.build/Objects-normal/x86_64/solverc.d
@@ -0,0 +1,12 @@
+dependencies: /Users/yifanliu/Developer/sd/sdlb/solverc.c \
+  /Users/yifanliu/Developer/sd/sdlb/prob.h \
+  /Users/yifanliu/Developer/sd/sdlb/sdconstants.h \
+  /Users/yifanliu/Applications/IBM/ILOG/CPLEX_Studio126/cplex/include/ilcplex/cplex.h \
+  /Users/yifanliu/Applications/IBM/ILOG/CPLEX_Studio126/cplex/include/ilcplex/cpxconst.h \
+  /Users/yifanliu/Developer/sd/sdlb/sdglobal.h \
+  /Users/yifanliu/Developer/sd/sdlb/cell.h \
+  /Users/yifanliu/Developer/sd/sdlb/soln.h \
+  /Users/yifanliu/Developer/sd/sdlb/quad.h \
+  /Users/yifanliu/Developer/sd/sdlb/solver.h \
+  /Users/yifanliu/Developer/sd/sdlb/utility.h \
+  /Users/yifanliu/Developer/sd/sdlb/log.h
